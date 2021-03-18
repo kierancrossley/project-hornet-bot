b15283779ec50fe075108a554aa1022367e87ba7
kierancrossley <47427240+kierancrossley@users.noreply.github.com>
2021-03-18 18:29:32 +0000

    Retry fix

--- commands/general/server.js
+++ commands/general/server.js
@@ -4,19 +4,7 @@ module.exports = class MeowCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'meow',
-			aliases: ['kitty-cat'],
-			group: 'first',
-			memberName: 'meow',
-			description: 'Replies with a meow, kitty cat.',
-		});
-	}
-};
-
-module.exports = class MeowCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'meow',
-			group: 'first',
+			group: 'general',
 			memberName: 'meow',
 			description: 'Replies with a meow, kitty cat.',
 		});
