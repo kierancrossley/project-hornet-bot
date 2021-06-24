46c8209456b6f89ddec5fce547d07f5b542c3830
kierancrossley <47427240+kierancrossley@users.noreply.github.com>
2021-06-24 17:42:03 +0100

    Update status.js

--- commands/general/status.js
+++ commands/general/status.js
@@ -12,11 +12,10 @@ module.exports = class StatusCommand extends Command {
 	}
  
 	run(message) {
-        query.info("208.103.169.207", 27015, 2000)
-            .then(function(response){
-                console.log(response)
-            })
+        var status = query.info("208.103.169.207", 27015, 2000)
+            .then(console.log)
             .catch(console.log)
             .then(query.close)
+        return message.say(status)
 	}
 };
