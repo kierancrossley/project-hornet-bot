255e50fee171b7f5e0bcc5ab19a8bcc18bca6662
kierancrossley <47427240+kierancrossley@users.noreply.github.com>
2021-06-24 20:05:04 +0100

    Update status.js

--- commands/server/status.js
+++ commands/server/status.js
@@ -11,7 +11,7 @@ module.exports = class StatusCommand extends Command {
 			description: 'Replies with server status',
 		});
 	}
- 
+    
     run(message) {
         query
             .info("208.103.169.207", 27015, 2000)
