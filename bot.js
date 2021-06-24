aa5edc912558f60f3b23fc760a18476ccb9330db
kierancrossley <47427240+kierancrossley@users.noreply.github.com>
2021-06-24 20:23:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const {CommandoClient} = require('discord.js-commando');
 const path = require('path');
+const ip = process.env.SERVER_IP, port = parseInt(process.env.SERVER_PORT)
 
 const client = new CommandoClient({
 	commandPrefix: process.env.PREFIX,
@@ -22,5 +23,17 @@ client.once('ready', () => {
 });
 
 client.on('error', console.error);
+
+let players = null;
+setInterval(() => {
+    query.info(ip, port, 2000)
+        .then(data => {
+            if (players !== data.playersnum) {
+                client.user.setActivity(`with ${data.playersnum} players`);
+                players = data.playersnum;
+            }
+        })
+		.catch(console.error)
+}, 5000);
 
 client.login(process.env.TOKEN);
