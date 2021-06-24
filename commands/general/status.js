79142a93c844d560ee88eb1700d435d932769114
kierancrossley <47427240+kierancrossley@users.noreply.github.com>
2021-06-24 17:32:17 +0100

    Update status.js

--- commands/general/status.js
+++ commands/general/status.js
@@ -14,6 +14,7 @@ module.exports = class StatusCommand extends Command {
 	run(message) {
         query.info("208.103.169.207", 27015, 2000)
             .then(function(response){
+                await response
                 console.log(response)
                 query.close
                 return message.say(response)
