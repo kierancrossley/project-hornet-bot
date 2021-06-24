d2388bb6311c1b9f843a45ccfeb358ad4beddd36
kierancrossley <47427240+kierancrossley@users.noreply.github.com>
2021-06-24 16:44:46 +0100

    Change files

--- commands/general/server.js
+++ commands/general/server.js
@@ -1,4 +1,3 @@
-const query = require("source-server-query");
 const {Command} = require('discord.js-commando');
 
 module.exports = class MeowCommand extends Command {
@@ -13,24 +12,5 @@ module.exports = class MeowCommand extends Command {
 
 	run(message) {
 		return message.say('Meow!');
-	}
-};
-
-module.exports = class StatusCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'status',
-			group: 'general',
-			memberName: 'status',
-			description: 'Replies with server status.',
-		});
-	}
-
-	run(message) {
-		query.info("208.103.169.207", 27015, 2000)
-			.then(console.log)
-			.catch(console.log)
-			.then(query.close)
-			.then(message.say);
 	}
 };
---
+++ commands/general/status.js
@@ -0,0 +1,21 @@
+const {Command} = require('discord.js-commando');
+const query = require("source-server-query");
+
+module.exports = class StatusCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'status',
+			group: 'general',
+			memberName: 'status',
+			description: 'Replies with server status.',
+		});
+	}
+
+	run(message) {
+		query.info("208.103.169.207", 27015, 2000)
+			.then(console.log)
+			.catch(console.log)
+			.then(query.close)
+			.then(message.say);
+	}
+};
